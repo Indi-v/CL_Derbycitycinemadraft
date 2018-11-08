25c84fa1efee5a8f0cd79ba492d0dfb317c8a843
Travis Davis <TDavis@corp.skyetech.net>
2018-11-07 20:06:19 -0500

    Added some actual style

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var $movieTable = $('#movieTable');
 
 
 function buildMovieTable () {
-
-    var row= "<tr><td>" + BoxOfficeDraft[idVal].Movie + "</td><td>" + BoxOfficeDraft[idVal].Owner + "</td><td>" + BoxOfficeDraft[idVal].DomesticGross; + '</td></tr>'
+    // Build a table row  
+    var row= "<tr><td>" + BoxOfficeDraft[idVal].Movie + "</td><td>" + BoxOfficeDraft[idVal].Owner + "</td><td> $" + (BoxOfficeDraft[idVal].DomesticGross/1000000).toFixed(2) + " Mil</td></tr>"
     $movieTable.append(row)
+    // Add the hiddenRow class to hide the column when on mobile
     var $hiddenRow = $('#movieTable td:nth-child(2n)')
     $hiddenRow.addClass('hiddenRow')
 }
@@ -25,6 +26,8 @@ var $scoreTable= $('#scoreTable');
 
 
 $scoreTable.append('<tr><th>Player</th><th>Current Total</th></tr>')
-            .each('draftdata.Score', function(key , value){
-                    $scoreTable.append('<tr><td>' + key + "</td><td>" + key.Gross + '</td></tr>') 
+$.each(Score, function(key , value){
+                console.log("test ", "$" +  (value.Gross/1000000).toFixed(2) + " Mil" )
+                console.log("val", value.Gross)
+                    $scoreTable.append('<tr><td>' + key + "</td><td>" + "$" +  (value.Gross/1000000).toFixed(2) + " Mil"+ '</td></tr>') 
                 });
