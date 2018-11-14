f59db31fd8b805f0f91d4318008e0c8bb7ed5fee
Travis Davis <TDavis@corp.skyetech.net>
2018-11-14 14:45:20 -0500

    Setup the links for movies

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var $scoreTable= $('#scoreTable');
 
 function buildMovieTable() {
     // Build a table row  
-    var row= "<tr><td>" + BoxOfficeDraft[idVal].Movie + "</td><td>" + BoxOfficeDraft[idVal].Owner + "</td><td> $" + (BoxOfficeDraft[idVal].DomesticGross/1000000).toFixed(2) + " Mil</td></tr>"
+    var row= "<tr><td><a href=" + BoxOfficeDraft[idVal].BOM_link + ">" + BoxOfficeDraft[idVal].Movie + "</td><td>" + BoxOfficeDraft[idVal].Owner + "</td><td> $" + (BoxOfficeDraft[idVal].DomesticGross/1000000).toFixed(2) + " Mil</td></tr>"
     $movieTable.append(row)
     // Add the hiddenRow class to hide the column when on mobile
     var $hiddenRow = $('#movieTable td:nth-child(2n)')
