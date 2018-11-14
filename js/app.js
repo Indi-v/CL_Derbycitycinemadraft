c0e6b61b4ccfe1d967887e9f84780df5ff1b48d9
Travis Davis <TDavis@corp.skyetech.net>
2018-11-14 08:43:47 -0500

    started readme

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@
 var BoxOfficeDraft = draftData.BoxOfficeDraft;
 var BodCount= Object.keys('draftData.BoxOfficeDraft').length;
 var $movieTable = $('#movieTable');
+var Score = draftData.Score;
+var $scoreTable= $('#scoreTable');
 
-
-function buildMovieTable () {
+function buildMovieTable() {
     // Build a table row  
     var row= "<tr><td>" + BoxOfficeDraft[idVal].Movie + "</td><td>" + BoxOfficeDraft[idVal].Owner + "</td><td> $" + (BoxOfficeDraft[idVal].DomesticGross/1000000).toFixed(2) + " Mil</td></tr>"
     $movieTable.append(row)
@@ -13,21 +14,18 @@ function buildMovieTable () {
     var $hiddenRow = $('#movieTable td:nth-child(2n)')
     $hiddenRow.addClass('hiddenRow')
 }
+
+
 for (i=1; i<BodCount; i++) {
     var idVal = i + 1000;
-    buildMovieTable()
-    
-
+    buildMovieTable();
 };
 
-var Score = draftData.Score;
-var $scoreTable= $('#scoreTable');
+
 
 
 
 $scoreTable.append('<tr><th>Player</th><th>Current Total</th></tr>')
-$.each(Score, function(key , value){
-                console.log("test ", "$" +  (value.Gross/1000000).toFixed(2) + " Mil" )
-                console.log("val", value.Gross)
-                    $scoreTable.append('<tr><td>' + key + "</td><td>" + "$" +  (value.Gross/1000000).toFixed(2) + " Mil"+ '</td></tr>') 
-                });
+$.each(Score, function buildScoreTable(key , value){
+    $scoreTable.append('<tr><td>' + key + "</td><td>" + "$" +  (value.Gross/1000000).toFixed(2) + " Mil"+ '</td></tr>') 
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Derby City Cinima Draft 
+
+##Description
+This is inspired by Diamond club podcast's Movie Draft league. 
+
+##Custom CSS Classes
+The class(es) I created are:
+
+1. class name ex( .main-info-section )
+.. what class does, ex( adds padding & changes background color of .main-info-section )
+
+2.
+
+##Custom JavaScript Functions
+The javascript functions I created are:
+
+1. buildMovieTable();
+  - Function used to itterate through my draftData and build the table out. It adds the class hidden-row to the 2nd column. That is used to hide the collumn on smaller screens. It is added back after the initial brake point via Media query. 
